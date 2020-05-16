aedc90062dd4c44d45f411bad924f10134952d63
CodyAdam <cody.adam@laposte.net>
2020-05-16 13:29:46 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Deco from "./Deco";
+import TitleBar from "./TitleBar";
+import "./css/index.css";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div id="App">
+                <MainWindow />
+            </div>
+        );
+    }
+}
+
+class MainWindow extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            functions: {
+                utilityFunction1: this.utilityFunction1,
+                utilityFunction2: this.utilityFunction2,
+                utilityFunction3: this.utilityFunction3,
+                utilityFunction4: this.utilityFunction4,
+            },
+        };
+    }
+
+    utilityFunction1() {
+        alert("The button does nothing.");
+    }
+
+    utilityFunction2() {
+        alert("(╯°□°)╯︵ ┻━┻");
+    }
+
+    utilityFunction3() {
+        document.getElementById("Decoration").style.opacity = 0.5;
+        document.body.style.backgroundColor = "rgb(80, 80, 82)";
+    }
+
+    utilityFunction4() {
+        document.getElementById("Decoration").style.opacity = 1;
+        document.body.style.backgroundColor = "#F2F2F3";
+    }
+
+    render() {
+        return (
+            <div id="MainWindow">
+                <TitleBar functions={this.state.functions} />
+                <Deco />
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root"),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
