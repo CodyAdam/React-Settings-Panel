ed41851e822b3b853cb5e576a1a0a99212d1c4dc
CodyAdam <cody.adam@laposte.net>
2020-05-19 22:25:35 +0200

    build

---
+++ docs/precache-manifest.647a5ce3272afa3c7f500cb06f526218.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c248e07d7ab4ed4150ba8d34d5acb3b7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "05c27dc9e75c92c5c887",
+    "url": "./static/css/main.5efb2af0.chunk.css"
+  },
+  {
+    "revision": "913b8cb525ed00be67af",
+    "url": "./static/js/2.babeaffe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.babeaffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05c27dc9e75c92c5c887",
+    "url": "./static/js/main.3a0618bf.chunk.js"
+  },
+  {
+    "revision": "92324899d47c6079579d",
+    "url": "./static/js/runtime-main.37259b6a.js"
+  }
+]);
